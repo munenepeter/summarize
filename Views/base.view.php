fde52417b05795ec580bf00ed32707d84aab6ddd
munenepeter <peternjega7@gmail.com>
2022-11-14 12:10:40 +0300

    change title

--- Views/base.view.php
+++ Views/base.view.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Summarizer</title>
     <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.3/dist/flowbite.min.css" />
     <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
